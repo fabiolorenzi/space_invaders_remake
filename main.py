d8364623e2dab82c6f98b0f83332b07c807a1078
fabiolorenzi <fabiolorenzi13975@gmail.com>
2024-04-02 12:30:37 +0100

    added player lateral movement and added borders

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load("media/characters/player.png")
 playerX = 370
 playerY = 480
+playerX_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 # Game loop
 
@@ -24,10 +25,20 @@ running = True
 while running:
     # Manage the screen before the characters
     screen.fill((0, 0, 0))
-    
+
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.2
+            elif event.key == pygame.K_RIGHT:
+                playerX_change = 0.2
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+    if playerX + playerX_change > 2 and playerX + playerX_change < 730:
+        playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
