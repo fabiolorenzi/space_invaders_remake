db5dd8db192ae165755c00d1d94b5d51cf27c80d
fabiolorenzi <fabiolorenzi13975@gmail.com>
2024-04-02 11:36:41 +0100

    added player icon to  the screen

--- main.py
+++ main.py
@@ -9,14 +9,25 @@ pygame.display.set_caption("Space Invaders Remake")
 icon = pygame.image.load("media/icons/main_icon.png")
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load("media/characters/player.png")
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 # Game loop
 
 running = True
 
 while running:
+    # Manage the screen before the characters
+    screen.fill((0, 0, 0))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+    player()
     pygame.display.update()
